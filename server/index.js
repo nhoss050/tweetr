7b15c377ed0b526148c730b54aa79c408ddffd33
nhoss050 <nima.hjavaheri@gmail.com>
2017-07-25 22:21:29 +0000

    new poer

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@
 
 // Basic express setup:
 require('dotenv').config();
-const PORT          = process.env.PORT || 8080;
+const PORT          = process.env.PORT || 8000;
 const express       = require("express");
 const bodyParser    = require("body-parser");
 const app           = express();
